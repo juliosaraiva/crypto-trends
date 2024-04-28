3c08c2dc393771989b59ad6326d3589ae7041f4c
Edson Costa <edson.13.10.2004@gmail.com>
2024-04-28 14:32:41 -0300

    fix: Pagination Correct

--- src/cryptor-trends-front/src/components/Navbar/index.tsx
+++ src/cryptor-trends-front/src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react";
+import { Image, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuToggle } from "@nextui-org/react";
 
 export default function NavbarHeader() {
   const menuItems = [
@@ -15,15 +15,19 @@ export default function NavbarHeader() {
 
       <NavbarContent className="sm:hidden pr-3" justify="center">
         <NavbarBrand>
-          <Image src='/images/logo-cryptotrends.svg' alt="" width={50} height={50} />
-          <p className="font-bold text-inherit">Crypto Trends</p>
+          <Link href="/">
+            <Image src='/images/logo-cryptotrends.svg' alt="" width={50} height={50} />
+            <p className="font-bold text-inherit">Crypto Trends</p>
+            </Link>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-        <Image src='/images/logo-cryptotrends.svg' alt="" width={50} height={50} />
-          <p className="font-bold text-inherit">Crypto Trends</p>
+      <NavbarBrand>
+          <Link href="/">
+            <Image src='/images/logo-cryptotrends.svg' alt="" width={50} height={50} />
+            <p className="font-bold text-inherit">Crypto Trends</p>
+            </Link>
         </NavbarBrand>
         <NavbarItem>
           <Link color="foreground" href="/#trends">
@@ -36,7 +40,7 @@ export default function NavbarHeader() {
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="/contact">
             Contact
           </Link>
         </NavbarItem>
@@ -44,20 +48,21 @@ export default function NavbarHeader() {
 
 
       <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
+        <NavbarItem>
+            <Link color="foreground" href="/#trends">
+              Trends
             </Link>
-          </NavbarMenuItem>
-        ))}
+          </NavbarItem>
+        <NavbarItem>
+            <Link color="foreground" href="/about">
+              About
+            </Link>
+          </NavbarItem>
+        <NavbarItem>
+            <Link color="foreground" href="/contact">
+              Contact
+            </Link>
+          </NavbarItem>
       </NavbarMenu>
     </Navbar>
   );
