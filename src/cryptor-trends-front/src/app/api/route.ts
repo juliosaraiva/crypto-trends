c2f7341a538a742849f6bb81f96204f4a1f3aa49
Edson Costa <edson.13.10.2004@gmail.com>
2024-04-29 23:38:05 -0300

    feat: more data mockend

--- src/cryptor-trends-front/src/app/api/route.ts
+++ src/cryptor-trends-front/src/app/api/route.ts
@@ -50,6 +50,114 @@ export async function GET() {
       timestamp: new Date("2024-04-23"),
       trend: "Low"
     },
+    {
+      coin_id: "1",
+      name: "Moeda1",
+      symbol: "MOED",
+      rank: 4,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "2",
+      name: "Moeda2",
+      symbol: "MOED",
+      rank: 5,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "3",
+      name: "Moeda3",
+      symbol: "MOED",
+      rank: 6,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "4",
+      name: "Moeda4",
+      symbol: "MOED",
+      rank: 7,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "5",
+      name: "Moeda5",
+      symbol: "MOED",
+      rank: 8,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "6",
+      name: "Moeda6",
+      symbol: "MOED",
+      rank: 9,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "7",
+      name: "Moeda7",
+      symbol: "MOED",
+      rank: 10,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "8",
+      name: "Moeda8",
+      symbol: "MOED",
+      rank: 11,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
+    {
+      coin_id: "9",
+      name: "Moeda9",
+      symbol: "MOED",
+      rank: 12,
+      max_supply: 116018364,
+      circulating_supply: 116018364,
+      total_supply: 116018364,
+      price: 3000.00,
+      timestamp: new Date("2024-04-23"),
+      trend: "Low"
+    },
   ];
 
   return new Response(JSON.stringify(cryptoList), {
